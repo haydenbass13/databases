ed20d52820923a9f7c26afb79c8c3971629462b8
andrewpiao1 <andrew.piao1@gmail.com>
2019-05-01 23:24:41 -0400

    mySql structure written

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,47 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Messages (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  msg_body VARCHAR(140) NOT NULL,
+  user_id INTEGER(5) NOT NULL,
+  room_id INTEGER(5) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (room_id) REFERENCES rooms (id)
+);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE Messages ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE rooms ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE users ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO Messages (id,msg_body,user_id,room_id) VALUES
+-- ('','','','');
+-- INSERT INTO rooms (id,room_name) VALUES
+-- ('','');
+-- INSERT INTO users (id,user_name) VALUES
+-- ('','');
 
 
 
@@ -15,3 +50,10 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- INSERT INTO Messages
+-- VALUES(0, 'test test',
+-- (SELECT id FROM users WHERE user_name = 'Andy'),
+-- (SELECT id FROM rooms WHERE room_name = 'waiting room'));
+
+-- INSERT INTO users
+-- VALUES(0, 'Andy');
